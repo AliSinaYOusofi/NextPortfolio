206c348c62973e391b9bc2628b241be25e514f37
dev-ethar <tinayousofiali@gmail.com>
2022-10-28 15:14:27 -0700

    light mode done

--- job-portfolio/components/Navbar/LightDark.tsx
+++ job-portfolio/components/Navbar/LightDark.tsx
@@ -6,21 +6,26 @@ import {CiLight, CiDark} from 'react-icons/ci';
 const LightDark: NextPage = () => {
 
     const [theme, setTheme] = React.useState(false);
+    const [html, setHTML] = React.useState();
     // using useRef() to give that fade transition
-    let light = React.useRef<HTMLDivElement>();
-    let dark = React.useRef<HTMLDivElement>();
     
     // handle the theme click. change it 
-    const handleThemeClick = () : void => { 
-
+    const handleThemeClick = (e : any)  => { 
+        e.preventDefault();
         setTheme(previouseTheme => !previouseTheme);
+        
+        let target = (e.target as Element);
+        target.classList.toggle("fade2");   
     }
+    
+    
     return (
         <div className="w-fit md:p-4 p-2 relative bg-gray-400 backdrop backdrop-blur-lg rounded-full
             cursor-pointer mr-10 md:mr-20 after:-z-10 after:bg-gray-500 after:absolute after:w-20 after:h-0
-            after:top-0 after:rounded-full after:-left-4 outline-none after:outline-none after:border-none after:backdrop-blur-lg  hover:after:w-20 hover:after:h-28 after:transition-all after:duration-300 overflow-hidden transition-all duration-300 hover:animate-none" onClick={handleThemeClick}>
+            after:top-0 hover:scale-105 after:rounded-full after:-left-4 outline-none after:outline-none after:border-none after:backdrop-blur-lg  hover:after:w-20 hover:after:h-28 after:transition-all after:duration-300 overflow-hidden transition-all duration-300 hover:animate-none" 
+            onClick={handleThemeClick}>
             {
-                theme ? <CiDark  className="text-black md:text-xl text-md transition-all duration-300 z-10" /> : <CiLight  className="text-black md:text-xl text-md transition-all duration-300"  />
+                theme ? <CiDark   className="fade2 text-black md:text-xl text-md transition-all duration-300 z-10" /> : <CiLight  className="text-black md:text-xl text-md transition-all duration-300 fade1"  />
             }
         </div>
     )
