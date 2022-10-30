57ad7c9b83d2e9fd20237f2ee367d3d25c1ce6e9
dev-ethar <tinayousofiali@gmail.com>
2022-10-30 09:26:22 -0700

    menu done

--- job-portfolio/components/Menu.tsx/MenuItem.tsx
+++ job-portfolio/components/Menu.tsx/MenuItem.tsx
@@ -1,15 +1,39 @@
 import { NextPage } from "next";
 import React from "react";
 import {CgProfile} from 'react-icons/cg';
+import {AiFillProject, AiOutlineHome} from 'react-icons/ai';
+import {GrContactInfo} from 'react-icons/gr';
+import {RiContactsLine} from 'react-icons/ri';
 
 const MenuItem: NextPage = () => {
     return(
-        <div className="hidden md:flex bg-black/30 absolute top-[17rem] right-[30rem]
-        justify-center items-center py-2 px-4">
-            <div className="justify-center items-center flex-col">
-               <div className="flex items-center justify-center"><CgProfile /></div>
+        <div className="menus flex bg-black/30 fixed top-[80%] right-[30rem]
+        justify-center items-center p-6 rounded-md gap-x-10 z-10 transition-all duration-300">
+            
+            <div className="menu justify-center items-center flex-col transition-all duration-300
+            bg-[#535353]  p-2 rounded-lg cursor-pointer">
+               <div className="flex items-center justify-center text-2xl"><CgProfile /></div>
                 <p>About</p>
             </div>
+            
+            <div className="menu justify-center items-center flex-col transition-all duration-300
+            bg-[#535353] p-2 rounded-lg cursor-pointer">
+               <div className="flex items-center justify-center text-2xl text-gray-400"><RiContactsLine /></div>
+                <p>Contact</p>
+            </div>
+            
+            <div className="menu justify-center items-center flex-col transition-all duration-300
+            bg-[#535353] p-2 rounded-lg cursor-pointer">
+               <div className="flex items-center justify-center text-2xl"><AiFillProject /></div>
+                <p>Projects</p>
+            </div>
+            
+            <div className="menu justify-center items-center flex-col transition-all duration-300
+            bg-[#535353] p-2 rounded-lg cursor-pointer">
+               <div className="flex items-center justify-center text-2xl"><AiOutlineHome /></div>
+                <p>Home</p>
+            </div>
+            
         </div>
     );
 }
