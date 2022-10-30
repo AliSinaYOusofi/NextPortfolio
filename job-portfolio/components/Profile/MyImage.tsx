524e2af0299a5a704c13fb49855240596b337ad6
dev-ethar <tinayousofiali@gmail.com>
2022-10-30 04:07:13 -0700

    profile comp done

--- job-portfolio/components/Profile/MyImage.tsx
+++ job-portfolio/components/Profile/MyImage.tsx
@@ -10,7 +10,7 @@ const MyImage: NextPage = () => {
         if(ref.current) {
             if (!document.fullscreenElement) {
                 ref.current.requestFullscreen({ navigationUI: "auto" })
-                    .catch(error => alert("something is wrong with you browser"));
+                    .catch(() => alert("something is wrong with you browser"));
             }
             else {
                 document.exitFullscreen();
@@ -20,7 +20,7 @@ const MyImage: NextPage = () => {
 
     return (
         <>
-            <div className="" ref={ref} onClick={makeFullScreen}>
+            <div className="w-fit cursor-pointer" ref={ref} onClick={makeFullScreen}>
                     <Image
                         src={me}
                         alt="mena"
@@ -28,7 +28,8 @@ const MyImage: NextPage = () => {
                         height={400}
                         object-fit="contain"
                         loading='eager'
-                        className="object-cover w-[15rem] h-[15rem] rounded-md relative top-24 md:ml-[9rem] ml-24"
+                        className="object-cover w-[15rem] h-[15rem] rounded-md relative top-24 md:ml-[9rem] ml-24
+                        transiti transition-all duration-300"
                     />
                 </div>
         </>
