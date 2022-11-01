d0cd257cb610963a91e8d9adee126723862991b2
dev-ethar <tinayousofiali@gmail.com>
2022-11-01 06:12:21 -0700

    done langs

--- job-portfolio/components/Profile/Languages.tsx
+++ job-portfolio/components/Profile/Languages.tsx
@@ -3,23 +3,25 @@ import React from "react";
 
 const Languages: NextPage = () => {
     const counter: number = 90;
+
     return (
-        <div className="w-full flex justify-center items-center gap-x-10">
+        <div className="w-full flex justify-center items-center gap-x-10  py-2">
             
-            
-            
+            <div className=" w-fit flex flex-col items-center justify-center
+            ">
+                <div className="bg-black/30 rounded-full py-3 px-[4px] text-center  w-fit border-2 border-[gray]">100%</div>
+                <i className="text-sm">Persian</i>
+            </div>
             <div className=" w-fit flex flex-col items-center justify-center">
-                <p className="bg-black/30 rounded-full p-2 text-center border-2 w-fit">{counter}%</p>
+                <div className="bg-black/30 rounded-full text-center w-fit py-3 px-[10px] border-2 border-[gray]">90%</div>
                 <i className="text-sm">English</i>
             </div>
             <div className=" w-fit flex flex-col items-center justify-center">
-                <p className="bg-black/30 rounded-full p-2 text-center border-2 w-fit">{counter}%</p>
-                <i className="text-sm">English</i>
+                <div className="bg-black/30 rounded-full  text-center w-fit py-3 px-[10px] border-2 border-[gray]">70%</div>
+                <i className="text-sm">Urdu</i>
             </div>
-            <div className=" w-fit flex flex-col items-center justify-center">
-                <p className="bg-black/30 rounded-full p-2 text-center border-2 w-fit">{counter}%</p>
-                <i className="text-sm">English</i>
-            </div>
+
+            
         </div>
     );
 }
