aa59f639ff669872551ed720249585b057de0f51
dev-ethar <tinayousofiali@gmail.com>
2022-11-04 23:59:00 -0700

    done links all are working fine

--- job-portfolio/components/Projects/Projects.tsx
+++ job-portfolio/components/Projects/Projects.tsx
@@ -15,14 +15,14 @@ const Projects: NextPage = () => {
         <>  
             <h1 className="text-center mt-10 text-3xl capitalize"> Some of my projects Projects </h1>
             <div className="w-[90%] ml-auto mr-auto h-full flex flex-row flex-wrap justify-center gap-x-5 items-cent">
-                <ProjectCards name={"Spotfiy Clone"} image={spotify} details={"Build with Reactjs, styled-components, javascript with Spotfiy Provided API."}/>
-                <ProjectCards name={"OpenSea Clone"} image={nft} details={"Built with Nextjs, TailwindCss, CSS3, and javascript. It uses nftport.xyz provided API."}/>
-                <ProjectCards name={"Netflix Clone"} image={netflix} details={"Only front-end Clone of Netflix Made with Reactjs, tailwindcss, CSS3, HTML, Javascript. "}/>
-                <ProjectCards name={"Dictionary"} image={dict} details={"Dictionary Made with Reactjs, tailwindcss, CSS3, HTML, Javascript. By API provided by dictionary.com."}/>
-                <ProjectCards name={"Prev Portfolio"} image={port} details={"My Previous Portfolio made with Reactjs, styled-components, CSS3, and javascript."}/>
-                <ProjectCards name={"Netflix"} image={fullstack} details={"For front-end I used Reactjs, tailwind and for backend MYSQL, Nodejs, Expressjs."}/>
-                <ProjectCards name={"Small Projects"} image={dev} details={"12-10 Simple Projects like weather app, tick-tack toe, one page gym and more ... ."}/>
-                <ProjectCards name={"User Management"} image={mongodb} details={"Full-stack user management system. Built with Nextjs, tailwind and for backend MongoDB, Nodejs, Expressjs."}/>
+                <ProjectCards link={"https://github.com/AliSinaYOusofi/SpotfiyClone"} name={"Spotfiy Clone"} image={spotify} details={"Build with Reactjs, styled-components, javascript with Spotfiy Provided API."}/>
+                <ProjectCards link={"https://github.com/AliSinaYOusofi/NFT"} name={"OpenSea Clone"} image={nft} details={"Built with Nextjs, TailwindCss, CSS3, and javascript. It uses nftport.xyz provided API."}/>
+                <ProjectCards link={"https://github.com/AliSinaYOusofi/NetflixClone"} name={"Netflix Clone"} image={netflix} details={"Only front-end Clone of Netflix Made with Reactjs, tailwindcss, CSS3, HTML, Javascript. "}/>
+                <ProjectCards link={"https://github.com/AliSinaYOusofi/Dictionary"} name={"Dictionary"} image={dict} details={"Dictionary Made with Reactjs, tailwindcss, CSS3, HTML, Javascript. By API provided by dictionary.com."}/>
+                <ProjectCards link={"https://github.com/AliSinaYOusofi/portfolio"} name={"Prev Portfolio"} image={port} details={"My Previous Portfolio made with Reactjs, styled-components, CSS3, and javascript."}/>
+                <ProjectCards link={"https://github.com/AliSinaYOusofi/FullStack-MovieWebApp"} name={"Netflix"} image={fullstack} details={"For front-end I used Reactjs, tailwind and for backend MYSQL, Nodejs, Expressjs."}/>
+                <ProjectCards link={"https://github.com/AliSinaYOusofi/SmallProjets"} name={"Small Projects"} image={dev} details={"12-10 Simple Projects like weather app, tick-tack toe, one page gym and more ... ."}/>
+                <ProjectCards link={"https://github.com/AliSinaYOusofi/mongoose"} name={"User Management"} image={mongodb} details={"Full-stack user management system. Built with Nextjs, tailwind and for backend MongoDB, Nodejs, Expressjs."}/>
             </div>
         </>
     );
